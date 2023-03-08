2456a3f89fd82af2e97b4b4664898cf4d8f73d3f
mod.Mars <malejandrosalcido@gmail.com>
2023-03-08 01:47:18 -0700

    Create initial webpack setup

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{const e=document.querySelector("#content"),t=document.createElement("div"),n=document.createElement("header");t.textContent="The interplanetary diner is an incredible experience from within the stars!. You will get to experience the voidness of space while enjoying a hearty highly processed meal.",n.textContent="Home",e.append(n,t)})();
